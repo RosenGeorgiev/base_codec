38051b517cd8edf8ae71907b0323923ddc3826e5
Rosen Georgiev <rosengeorgiev93@gmail.com>
2020-12-20 15:44:55 +0200

    Update docs

--- include/base_codec/base16.hpp
+++ include/base_codec/base16.hpp
@@ -13,17 +13,25 @@ namespace base_codec
 /**
  * @brief Encodes a vector of bytes as a Base16 string.
  *
- * @param[in] a_input Bytes to encode.
- * @param[in] a_ec std::error_code that gets set if a problem occurs.
+ * @param[in] a_data Bytes to encode.
+ * @param[in][out] a_ec std::error_code that gets set if a problem occurs.
  *
  * @returns std::string Base16 representation of the data.
  */
 auto base16_encode(
-    std::vector<std::uint8_t> const& a_input,
+    std::vector<std::uint8_t> const& a_data,
     std::error_code& a_ec
 )
 -> std::string;
 
+/**
+ * @brief Decodes a Base16 encoded string.
+ *
+ * @param[in] a_data Base64 encoded string to decode.
+ * @param[in][out] a_ec std::error_code that gets set if a problem occurs.
+ *
+ * @returns std::vector<std::uint8_t> Byte representation of the decoded string.
+ */
 auto base16_decode(
     std::string_view const& a_data,
     std::error_code& a_ec,
